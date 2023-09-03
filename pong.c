5d56aaf24f44309ccfba82b8318d0da729d9e507
DrakeBrewer <drake@netstorms.com>
2023-09-03 08:29:48 +0000

    added menu banner and difficulty screen and struct. Working on ability to choose difficulty

--- pong.c
+++ pong.c
@@ -9,12 +9,22 @@
 
 struct ppball the_Ball;
 
+struct difficulty {int level; char *name;};
+struct difficulty difficulties[] = {
+    {0, "I'm Too Young To Die"},
+    {1, "Hey, Not Too Rough"},
+    {2, "Hurt Me Plenty"},
+    {3, "Ultra-Violence"},
+    {4, "Nightmare!"}
+};
+
 void setUp();
 void wrapUp();
 int setTicker(int);
 int bounceOrLose(struct ppball *);
 void ballMove(int);
 void drawCourt();
+void chooseDifficulty(int);
 
 int main() {
     int c;
@@ -22,16 +32,78 @@ int main() {
     setUp();
 
     while ((c = getchar()) != 'Q') {
-        if (c == 'f') the_Ball.x_ttm--;
-        else if (c == 's') the_Ball.x_ttm++;
-        else if (c == 'F') the_Ball.y_ttm--;
-        else if (c == 'S') the_Ball.y_ttm++;
+        // if (c == 'f') the_Ball.x_ttm--;
+        // else if (c == 's') the_Ball.x_ttm++;
+        // else if (c == 'F') the_Ball.y_ttm--;
+        // else if (c == 'S') the_Ball.y_ttm++;
+
+        switch (c)
+        {
+            case 13:
+                clear();
+                drawCourt();
+                difficultyMenu();
+                chooseDifficulty(&c);
+                refresh();
+                break;
+            
+            default:
+                break;
+        }
     }
 
     wrapUp();
 }
 
 void setUp() {
+    initscr();
+    clear();
+
+    drawCourt();
+    drawPong();
+    move(25,52);
+    addstr("Press Enter to play");
+    move(LINES-1,0);
+
+    refresh();
+
+    noecho();
+    crmode();
+}
+
+void chooseDifficulty(int select) {
+    int selectedDif = 0;
+    int cursorPos = 12;
+    
+    mvaddch(cursorPos,50,'#');
+    move(LINES-1,0);
+    // refresh();
+
+    switch (select)
+    {
+        case 'j':
+            // if (cursorPos < 20)
+            // {
+                selectedDif++;
+                mvaddch(cursorPos += 2,50,'#');
+                refresh();
+                move(LINES-1,0);
+            // }
+        case 'k':
+            if (cursorPos > 12) 
+            {
+                selectedDif--;
+                mvaddch(cursorPos -= 2,50,'#');
+                refresh();
+                move(LINES-1,0);
+            }
+        case 13:
+            // selectDifficulty(selectedDif);
+            break;
+    }
+}
+
+void startGame() {
     void ballMove(int);
 
     the_Ball.y_pos = Y_INIT;
@@ -53,7 +125,6 @@ void setUp() {
 
     signal(SIGALRM, ballMove);
     setTicker(1000/TICKS_PER_SEC);
-
 }
 
 void wrapUp() {
@@ -129,6 +200,10 @@ int setTicker( int nMsecs )
 	return setitimer(ITIMER_REAL, &newTimeset, NULL);
 }
 
+
+// Drawing functions
+// ============================================
+
 // draws the borders for the court
 void drawCourt() {
 
@@ -141,5 +216,84 @@ void drawCourt() {
         addstr("*");
         move(31, ii);
         addstr("*");
+    }
+}
+
+void difficultyMenu() {
+    move(8,52);
+	addstr("Choose a Difficulty!");
+    int yVal = 12;
+    move(yVal,52);
+    for (int ii=0; ii<=4; ii++) {
+        mvaddstr(yVal,52,difficulties[ii].name);
+        yVal+=2;  
+    }
+	move(LINES-1,0);
+}
+
+// draws the title of the game
+void drawPong() {
+    // P
+    drawLine(10, 10, 31, "#", 1);
+    drawLine(10, 10, 32, "#", 1);
+    drawLine(11, 3, 40, "#", 1);
+    drawLine(11, 3, 41, "#", 1);
+
+    drawLine(31, 9, 10, "#", 0);
+    drawLine(31, 9, 14, "#", 0);
+    
+    // O
+    drawLine(11, 8, 45, "#", 1);
+    drawLine(11, 8, 46, "#", 1);
+    drawLine(11, 8, 56, "#", 1);
+    drawLine(11, 8, 57, "#", 1);
+
+    drawLine(47, 9, 10, "#", 0);
+    drawLine(47, 9, 19, "#", 0);
+
+    // N
+    drawLine(10, 10, 61, "#", 1);
+    drawLine(10, 10, 62, "#", 1);
+    drawLine(10, 10, 72, "#", 1);
+    drawLine(10, 10, 73, "#", 1);
+
+    for (int ii=0; ii<10; ii++) {
+        move(10+ii,63+ii);
+        addstr("#");
+    }
+
+    // G
+    drawLine(11, 8, 77, "#", 1);
+    drawLine(11, 8, 78, "#", 1);
+    
+    drawLine(11, 2, 87, "#", 1);
+    drawLine(11, 2, 88, "#", 1);
+
+    drawLine(16, 3, 87, "#", 1);
+    drawLine(16, 3, 88, "#", 1);
+
+    drawLine(79, 9, 10, "#", 0);
+    drawLine(79, 9, 19, "#", 0);
+    
+    drawLine(85, 2, 16, "#", 0);
+
+}
+
+// draws a verticle line using a specified string.
+// verticle: dir = 1
+// horizontal: dir = 0
+void drawLine(int start, int len, int constCord, char *str, int dir) {
+
+    if (dir == 1) {
+        for (int ii = 0; ii < len; ii++) {
+            move(start+ii,constCord);
+            addstr(str);
+        }
+        refresh();
+    } else if (dir == 0) {
+        for (int ii = 0; ii < len; ii++) {
+            move(constCord,start+ii);
+            addstr(str);
+        }
     }
 }
